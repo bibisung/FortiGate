8ab75558ed1058bda92c70b6e52ab6cee7dd2fb9
Pillsang Sung <schmidtiana95@gmail.com>
2021-10-25 14:13:08 +0900

    update_202110251413

--- fortigate.js
+++ fortigate.js
@@ -1,30 +1,77 @@
+const API_KEY = 'jyrjmbGdcc5n1y7H5qqH5gzr56dy94'
+const GET_USER_URL = 'https://192.168.1.99/api/v2/cmdb/user/local/'
+const GET_POLICY_URL = 'https://192.168.1.99/api/v2/cmdb/firewall/policy/'
+const CREATE_USER_URL = 'https://192.168.1.99/api/v2/cmdb/user/local/'
+const GET_CONFIG_URL = 'https://192.168.1.99/api/v2/monitor/system/config/backup/?scope=global'
+const buttonEl = document.querySelector('.btn')
+const CurrentTime = Date.now()
+
 // FortiGate 모든 User 정보 가져오기
-const getUserUrl = 'https://192.168.1.99/api/v2/cmdb/user/local/'
-
-const getUserRes = fetch(getUserUrl, {
-    method: 'GET',
-    headers: {
-      'Authorization': 'Bearer jyrjmbGdcc5n1y7H5qqH5gzr56dy94',
-      'Accept': 'application/json',
-      'Origin': 'https://192.168.1.99'
-    },
-    mode: 'cors',
-    cache: 'default'
-}).then(getUserRes => {return getUserRes.json();})
-console.log(getUserRes)
-
+const GET_USER_RES = fetch(GET_USER_URL, {
+  method: 'GET',
+  headers: {
+    'Authorization': `Bearer ${API_KEY}`,
+    'Accept': 'application/json',
+    'Origin': 'https://localhost:1234'
+  },
+  mode: 'cors',
+  cache: 'default'
+}).then(GET_USER_RES => GET_USER_RES.json())
+console.log(GET_USER_RES)
 
 // FortiGate IPv4 Policy 가져오기
-const getPolicyUrl = 'https://192.168.1.99/api/v2/cmdb/firewall/policy/'
+const GET_POLICY_RES = fetch(GET_POLICY_URL, {
+  method: 'GET',
+  headers: {
+    'Authorization': `Bearer ${API_KEY}`,
+    'Accept': 'application/json',
+    'Origin': 'https://localhost:1234'
+  },
+  mode: 'cors',
+  cache: 'default'
+}).then(GET_POLICY_RES => GET_POLICY_RES.json())
+console.log(GET_POLICY_RES)
 
-const getPolicyRes = fetch(getPolicyUrl, {
-    method: 'GET',
-    headers: {
-      'Authorization': 'Bearer jyrjmbGdcc5n1y7H5qqH5gzr56dy94',
-      'Accept': 'application/json',
-      'Origin': 'https://192.168.1.99'
-    },
-    mode: 'cors',
-    cache: 'default'
-}).then(getPolicyRes => {return getPolicyRes.json();})
-console.log(getPolicyRes)
+// User 생성
+// const CREATE_USER = fetch(CREATE_USER_URL, {
+//   method: 'POST',
+//   headers: {
+//     'Authorization': `Bearer ${API_KEY}`,
+//     'Accept': 'application/json',
+//     'Origin': 'https://localhost:1234'
+//   },
+//   body: JSON.stringify({
+//     'name': 'user001',
+//     'status': 'enable',
+//     'type': 'password',
+//     'two-factor': 'disable',
+//     'sms-server': 'fortiguard',
+//     'auth-concurrent-override': 'disable',
+//     'passwd': 'user001'
+//   }),
+//   mode: 'cors',
+//   cache: 'default'
+// }).then((response) => response.json())
+//   .then((data) => console.log(data))
+
+if(buttonEl) {
+  buttonEl.addEventListener('click', function () {
+    const GET_CONFIG_RES = fetch(GET_CONFIG_URL, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${API_KEY}`,
+        'Origin': 'https://localhost:1234'
+      },
+      mode: 'cors',
+      cache: 'default'
+    }).then(GET_CONFIG_RES => GET_CONFIG_RES.blob())
+      .then(blob => {
+        const url = window.URL.createObjectURL(blob)
+        const a = document.createElement('a')
+        a.href = url
+        a.download = `${CurrentTime}.conf`
+        a.click()
+        a.remove()
+      })
+  })
+}
